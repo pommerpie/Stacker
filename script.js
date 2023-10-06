8f10c4a2117a6d7e0bdd45b281fca2be57495b69
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2023-10-05 20:40:03 -0400

    added more accurate variables for the addItem button and some appends

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ function addItem () {
         var mediaType = data.result[1].type
         var releaseDate = data.result[1].firstAirYear
         var recentDate = data.result[1].lastAirYear
-        var genres = data.result[1].genres
+        var genreFirst = data.result[1].genres[0].name
+        var genreSecond = data.result[1].genres[1].name
         var creators = data.result[1].creators
-        console.log(releaseDate)
-        console.log(recentDate)
-        console.log(creators)
+        var creatorFirst = creators.slice(0, 1)
+        var creatorSecond = creators.slice(1)
         $('.movie-back').append(title)
         $('.movie-back').append(mediaType)
         $('.movie-back').append(releaseDate + ' ' + recentDate)
-        $('.movie-back').append(genres)
-        $('.movie-back').append(creators)
+        $('.movie-back').append(genreFirst + ' ' + genreSecond)
+        $('.movie-back').append(creatorFirst + ' ' + creatorSecond)
     })
 }
