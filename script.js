1d9bd944d9971ec24b2c6d1328d4fe49cb64b1d9
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2023-10-05 20:21:08 -0400

    added more variables to the addItem button

--- script.js
+++ script.js
@@ -25,13 +25,19 @@ function addItem () {
     })
     .then(function(data){
         console.log(data)
+        var title = data.result[1].title
+        var mediaType = data.result[1].type
         var releaseDate = data.result[1].firstAirYear
         var recentDate = data.result[1].lastAirYear
+        var genres = data.result[1].genres
         var creators = data.result[1].creators
         console.log(releaseDate)
         console.log(recentDate)
         console.log(creators)
-        $('.movie-back').append(releaseDate + recentDate)
+        $('.movie-back').append(title)
+        $('.movie-back').append(mediaType)
+        $('.movie-back').append(releaseDate + ' ' + recentDate)
+        $('.movie-back').append(genres)
         $('.movie-back').append(creators)
     })
 }
