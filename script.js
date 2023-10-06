4cb47139831cba824f5c40ae0b989eff60c50d75
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2023-10-05 20:13:25 -0400

    added some data variables

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+var userSearch = 'One Piece'
+
+var movieNight = 'https://streaming-availability.p.rapidapi.com/search/title?country=us&show_type=all&output-language=en&rapidapi-key=ab82a71388mshb670a9456117badp107e60jsn596c04ae7f2f&title=' + userSearch
+
 $('#search').on('click', search)
+$('.addMovie').on('click', addItem)
 
 function search (event) {
-
     event.preventDefault()
     // var userSearch = $("input[name='search']")
-    
-    var movieNight = 'https://streaming-availability.p.rapidapi.com/search/title?country=us&show_type=all&output-language=en&rapidapi-key=ab82a71388mshb670a9456117badp107e60jsn596c04ae7f2f&title=' + userSearch
-    
     fetch(movieNight)
-    .then(function (response) {
+    .then(function (response){
         return response.json()
     })
-    .then(function (data) {
+    .then(function(data){
         console.log(data)
+    })
+}
 
+addItem()
+function addItem () {
+    fetch(movieNight)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        var releaseDate = data.result[1].firstAirYear
+        var recentDate = data.result[1].lastAirYear
+        var creators = data.result[1].creators
+        console.log(releaseDate)
+        console.log(recentDate)
+        console.log(creators)
+        $('.movie-back').append(releaseDate + recentDate)
+        $('.movie-back').append(creators)
     })
 }
